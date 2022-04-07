344806f598b4780fae72ebfa91772c7ba0d7f4b5
Honorine Ndom <honorinendzah@gmail.com>
2022-04-07 15:50:02 -0700

    changes done on components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import HelloDiv from "./components/HelloDiv";
+import React from 'react';
+import List from './components/List';
+import Nav from './components/Nav';
 
-function App() {
-  return <HelloDiv />;
+// Import our list of users from users.js
+import users from './users';
+
+// Pass users array to the List component as a prop
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <List users={users} />
+    </div>
+  );
 }
 
-export default App;
