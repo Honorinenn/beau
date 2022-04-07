cc81222df4ff264b22d35a34f5d37df843ae7afe
Honorine Ndom <honorinendzah@gmail.com>
2022-04-07 16:37:45 -0700

    changes done on components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import List from './components/List';
-import Nav from './components/Nav';
+import React from "react";
+import JSXVariables from "./components/JSXVariables";
 
-// Import our list of users from users.js
-import users from './users';
-
-// Pass users array to the List component as a prop
-export default function App() {
-  return (
-    <div>
-      <Nav />
-      <List users={users} />
-    </div>
-  );
+function App() {
+  return <JSXVariables />;
 }
+
+export default App;
 
